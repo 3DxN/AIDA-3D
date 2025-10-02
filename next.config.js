aeabe76ba0c939ab3ce948c1df5fc08023ed361c
ehtbanton <notnatheyam@gmail.com>
2025-10-02 14:15:11 +0000

    Replace deprecated next-transpile-modules with native transpilePackages for Next.js 15

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 // Openlayers is not compatible with NodeJS and therefore throws an error on
 // import because we're trying to use ES Modules in a NodeJS environment.
-// This is the fix.
+// In Next.js 15, use transpilePackages instead of next-transpile-modules
 // See: https://github.com/openlayers/openlayers/issues/10470
-const withTranspile = require('next-transpile-modules')(['ol'])
 
-module.exports = withTranspile({
+module.exports = {
+	// Use transpilePackages for Next.js 15 compatibility
+	transpilePackages: ['ol'],
 	typescript: {
 		// !! WARN !!
 		// Dangerously allow production builds to successfully complete even if
@@ -16,4 +17,4 @@ module.exports = withTranspile({
 		// Ignore ESLint errors during production builds
 		ignoreDuringBuilds: true,
 	},
-})
+}
