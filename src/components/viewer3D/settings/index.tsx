19e8f5e0bac4bf437d7a1b52f9cf9e4757ef1f6f
ehtbanton <notnatheyam@gmail.com>
2025-10-09 10:22:23 +0100

    fixed disappearing 3D viewer

--- src/components/viewer3D/settings/index.tsx
+++ src/components/viewer3D/settings/index.tsx
@@ -44,14 +44,23 @@ export default function Settings(props: {
 
 	const [isOpen, setIsOpen] = useState(true);
 
+	const handleToggle = (newState: boolean) => {
+		setIsOpen(newState);
+		if (renderer) {
+			setTimeout(() => {
+				resizeRendererToDisplaySize(renderer);
+				if (scene && camera) {
+					renderer.render(scene, camera);
+				}
+			}, 0);
+		}
+	};
+
 	return (
 		<>
 			{!isOpen && (
 				<button
-					onClick={() => {
-						setIsOpen(true);
-						if (renderer) resizeRendererToDisplaySize(renderer);
-					}}
+					onClick={() => handleToggle(true)}
 					className="rounded-bl-md hover:bg-gray-100 border-gray-200 shadow p-2 bg-white absolute top-0 right-0 inline-flex items-center text-gray-800 focus:outline-none focus:ring-2 focus:ring-teal-500"
 				>
 					<ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
@@ -60,12 +69,9 @@ export default function Settings(props: {
 			)}
 
 			{isOpen && (
-				<div className="bg-white border-l border-gray-200 h-screen shadow text-gray-800 flex flex-col divide-y overflow-y-auto">
+				<div className="bg-white border-l border-gray-200 h-full shadow text-gray-800 flex flex-col divide-y overflow-y-auto">
 					<button
-						onClick={() => {
-							setIsOpen(false);
-							if (renderer) resizeRendererToDisplaySize(renderer);
-						}}
+						onClick={() => handleToggle(false)}
 						className="w-48 flex justify-between hover:bg-gray-100 p-2 items-center focus:outline-none  ring-inset focus:ring-2 focus:ring-teal-500"
 					>
 						Settings
